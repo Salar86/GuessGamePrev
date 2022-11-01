0e56529840575ce628536de147b57c85b089e9a4
Salar86 <99397531+Salar86@users.noreply.github.com>
2022-11-01 11:43:50 +0100

    AlienGuesser Class added

---
+++ src/AlienGuesser.java
@@ -0,0 +1,34 @@
+public class AlienGuesser implements IGuesser{
+    AlienGuesser(){
+
+    }
+    @Override
+    public void yourTurn() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void newGame(int min, int max, String opponent, String type) {
+
+    }
+
+    @Override
+    public int makeGuess() {
+        return 0;
+    }
+
+    @Override
+    public void guessFeedback(Answer answer) {
+
+    }
+
+    @Override
+    public void endOfGame(int numberOfGuesses, String opponent) {
+
+    }
+}
