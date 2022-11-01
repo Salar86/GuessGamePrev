cfa39fd983691702517e0268b4c7a3c703c5b432
Salar86 <99397531+Salar86@users.noreply.github.com>
2022-11-01 11:27:12 +0100

    Merging Alienthinker

---
+++ src/AlienThinker.java
@@ -0,0 +1,26 @@
+public class AlienThinker implements IThinker{
+    @Override
+    public void yourTurn() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void newGame(int min, int max, String opponent, String type) {
+
+    }
+
+    @Override
+    public Answer evaluateGuess(int guess, String opponent) {
+        return null;
+    }
+
+    @Override
+    public void endOfGame(int numberOfGuesses, String opponent) {
+
+    }
+}
